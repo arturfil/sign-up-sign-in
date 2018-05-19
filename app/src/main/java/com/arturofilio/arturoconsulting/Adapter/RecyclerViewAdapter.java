b8ef68d2b560d038740941da6141ced29e4ab0ef
Arturo Filio Villa <arturfil@hotmail.com>
2018-05-19 00:38:49 -0400

    finished the list recycler view adapter, missing implementing the layout and view when user clicks the home button using a click listerner and HomeActivity setup

--- app/src/main/java/com/arturofilio/arturoconsulting/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/arturofilio/arturoconsulting/Adapter/RecyclerViewAdapter.java
@@ -1,12 +1,21 @@
 package com.arturofilio.arturoconsulting.Adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.arturofilio.arturoconsulting.Interface.ItemClickListener;
+import com.arturofilio.arturoconsulting.Model.Exercise;
 import com.arturofilio.arturoconsulting.R;
+
+import java.util.List;
 
 /**
  * Created by arturofiliovilla on 5/17/18.
@@ -15,21 +24,65 @@ import com.arturofilio.arturoconsulting.R;
 class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
 
-    private ImageView image;
-    private TextView text;
+    public ImageView image;
+    public TextView text;
 
     private ItemClickListener itemClickListener;
 
      public RecyclerViewHolder(View itemView) {
         super(itemView);
-//        image = (ImageView) itemView.findViewById(R.id.item_exercise);
+         image = (ImageView) itemView.findViewById(R.id.exercise_image);
+         text = (TextView) itemView.findViewById(R.id.exercise_name);
+
+         itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View view) {
+        itemClickListener.onClick(view, getAdapterPosition());
     }
 }
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+    private List<Exercise> exerciseList;
+    private Context context;
+
+    public RecyclerViewAdapter(List<Exercise> exerciseList, Context context) {
+        this.exerciseList = exerciseList;
+        this.context = context;
+    }
+
+
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_exercise, parent, false);
+
+        return new RecyclerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.image.setImageResource(exerciseList.get(position).getIamge_id());
+        holder.text.setText(exerciseList.get(position).getName());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View vew, int position) {
+                //Call to new Activity
+                Toast.makeText(context, "Click to " + exerciseList.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseList.size();
+    }
 }
--- app/src/main/java/com/arturofilio/arturoconsulting/Home/ListExercises.java
+++ app/src/main/java/com/arturofilio/arturoconsulting/Home/ListExercises.java
@@ -1,6 +1,16 @@
 package com.arturofilio.arturoconsulting.Home;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+
+import com.arturofilio.arturoconsulting.Adapter.RecyclerViewAdapter;
+import com.arturofilio.arturoconsulting.Model.Exercise;
+import com.arturofilio.arturoconsulting.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by arturofiliovilla on 5/17/18.
@@ -8,6 +18,32 @@ import android.app.Activity;
 
 public class ListExercises extends Activity {
 
+    List<Exercise> exerciseList = new ArrayList<>();
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_exercises);
 
+        initData();
+
+        recyclerView = (RecyclerView)findViewById(R.id.list_ex);
+        adapter = new RecyclerViewAdapter(exerciseList, getBaseContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initData() {
+
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+        exerciseList.add(new Exercise(R.drawable.chest_workout, "Chest Workout"));
+
+    }
 }
