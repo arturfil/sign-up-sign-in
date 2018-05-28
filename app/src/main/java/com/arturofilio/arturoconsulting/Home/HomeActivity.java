7fc6d14cd9b89014859b031f1575b47eebc0f57d
Arturo Filio Villa <arturfil@hotmail.com>
2018-05-27 21:48:23 -0400

    created the register layout and login layout, created the LoginActivity and RegisterActivity classes, missing the implementation

--- app/src/main/java/com/arturofilio/arturoconsulting/Home/HomeActivity.java
+++ app/src/main/java/com/arturofilio/arturoconsulting/Home/HomeActivity.java
@@ -1,15 +1,20 @@
 package com.arturofilio.arturoconsulting.Home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.arturofilio.arturoconsulting.Login.LoginActivity;
 import com.arturofilio.arturoconsulting.R;
 import com.arturofilio.arturoconsulting.Utils.BottomNavigationViewHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
@@ -18,6 +23,10 @@ public class HomeActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 0;
 
     private Context mContext = HomeActivity.this;
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListerner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,4 +49,62 @@ public class HomeActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+/*
+    ---------------------------------------------Firebase Auth ------------------------------------>
+     */
+
+    private void checkCurrentUser(FirebaseUser user) {
+        Log.d(TAG, "checkCurrentUser: checking if user is logged in");
+
+        /**
+         * check to see if the @param 'user' is logged in.
+         */
+        if(user == null) {
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase Auth");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListerner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                //check if the user is logged in
+                checkCurrentUser(user);
+
+                if (user != null) {
+                    //User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListerner);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthStateListerner != null) {
+            mAuth.addAuthStateListener(mAuthStateListerner);
+        }
+    }
+
 }
