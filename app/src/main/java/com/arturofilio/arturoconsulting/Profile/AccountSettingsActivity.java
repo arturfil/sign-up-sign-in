c8b928db79fa826071a29121e74a7caef2413328
Arturo Filio Villa <arturfil@hotmail.com>
2018-05-30 20:14:22 -0400

    added the logic to navigate to the sign out fragment, full auth is completed

--- app/src/main/java/com/arturofilio/arturoconsulting/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/arturofilio/arturoconsulting/Profile/AccountSettingsActivity.java
@@ -3,22 +3,31 @@ package com.arturofilio.arturoconsulting.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.arturofilio.arturoconsulting.R;
+import com.arturofilio.arturoconsulting.Utils.SectionsStatePagerAdapter;
 
 import java.util.ArrayList;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "AccountSettingsActivity";
+    private static final int ACTIVITY_NUM = 3;
 
     private Context mContext;
+
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,8 +35,11 @@ public class AccountSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_accountsettings);
         mContext = AccountSettingsActivity.this;
         Log.d(TAG, "onCreate: started");
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relLayout1);
 
         setupSettingsList();
+        setupFragments();
 
         //setup the backarraow for navigating back to "Profile Activity"
         ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
@@ -40,6 +52,18 @@ public class AccountSettingsActivity extends AppCompatActivity {
         });
     }
 
+    private void setupFragments() {
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new SignOutFragment(), getString(R.string.sign_out));
+    }
+
+    private void setViewPager(int fragmentNumber){
+        mRelativeLayout.setVisibility(View.GONE);
+        Log.d(TAG, "setViewPager: navigating to fragment #:" + fragmentNumber);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     private void setupSettingsList() {
         Log.d(TAG, "setupSettingsList: initialiting 'Account Settings' list");
         ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
@@ -50,5 +74,13 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: navigating to fragment: " + position);
+                setViewPager(position);
+            }
+        });
     }
 }
